ca39dbdea8b7caf25bc0b123d24fd56cb9e2b6cc
Kazuhiro Nomura <kazuhironomura43@gmail.com>
2026-05-06 11:25:09 +0200

    add owner.show page

--- src/Controller/OwnerController.php
+++ src/Controller/OwnerController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Restaurant;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -18,6 +21,19 @@ final class OwnerController extends AbstractController
     {
         return $this->render('owner/index.html.twig', [
             'restaurants' => $user->getRestaurants()->getValues(),
+        ]);
+    }
+
+    #[Route('/restaurant/{id}', name: 'app_restaurant_detail', requirements: ['id' => Requirement::DIGITS])]
+    public function show(#[CurrentUser] User  $user, Restaurant $restaurant): Response
+    {
+        if ($user->getId() !== $restaurant->getOwner()->getId()) {
+            throw $this->createAccessDeniedException();
+        }
+        return $this->render('owner/show.html.twig', [
+            'restaurant' => $restaurant,
+            'openingHours' => $restaurant->getOpeningHours()->getValues(),
+            'reservations' => $restaurant->getReservations()->getValues(),
         ]);
     }
 }
